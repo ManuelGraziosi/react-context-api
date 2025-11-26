94ea35688fd6be837db27a95eaa202d0d04e6cc9
ManuelGraziosi <manuel.graziosi@gmail.com>
2025-11-26 20:05:13 +0100

    feat: BudgetContext created

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,24 @@ import ChiSiamo from "./Pages/ChiSiamo";
 import Prodotti from "./Pages/Prodotti";
 import NotFound from "./Pages/NotFound";
 import DettagliProdotto from "./Pages/DettagliProdotto";
+import { BudgetContext } from "./contexts/BudgetContext";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/chi-siamo" element={<ChiSiamo />} />
-            <Route path="/prodotti" element={<Prodotti />} />
-            <Route path="/prodotti/:id" element={<DettagliProdotto />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <BudgetContext>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/chi-siamo" element={<ChiSiamo />} />
+              <Route path="/prodotti" element={<Prodotti />} />
+              <Route path="/prodotti/:id" element={<DettagliProdotto />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </BudgetContext>
     </>
   );
 }
---
+++ src/contexts/BudgetContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useContext, useState } from "react";
+
+const BudgetContext = createContext();
+
+function BudgetProvider({ children }) {
+  const [budgetMode, setBudgetMode] = useState(false);
+
+  return (
+    <BudgetContext.Provider
+      value={{
+        budgetMode,
+        setBudgetMode,
+      }}
+    >
+      {children}
+    </BudgetContext.Provider>
+  );
+}
+
+function useBudget() {
+  const context = useContext(BudgetContext);
+  return context;
+}
+
+export { BudgetContext, useContext };
